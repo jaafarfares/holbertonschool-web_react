8bd0d46eda906f1701006f0d4c9339d7db0b4c83
jaafar <jaafarferes@gmail.com>
2023-05-02 12:47:21 +0100

    0. Creating an interface for a student

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -5,16 +5,32 @@ interface Student {
     location: string
   }
   const student1: Student = {
-      firstName: "",
-      lastName: "",
-      age: 0,
-      location: ""
+      firstName: "john",
+      lastName: "smith",
+      age: 20,
+      location: "gabes"
   };
   
   const student2: Student = {
-    firstName: "",
-    lastName: "",
-    age: 0,
-    location: ""
+    firstName: "tom",
+    lastName: "..",
+    age: 26,
+    location: "tunis"
   };
  const studentsList: Student[] = [student1, student2];
+ const table = document.querySelector("#student-table");
+
+ studentsList.forEach((student) => {
+   const row = document.createElement("tr");
+   
+   const firstNameCell = document.createElement("td");
+   firstNameCell.textContent = student.firstName;
+   row.appendChild(firstNameCell);
+   
+   const locationCell = document.createElement("td");
+   locationCell.textContent = student.location;
+   row.appendChild(locationCell);
+   
+   table.appendChild(row);
+ });
+ 
