d53ab9eddc6bc7e312a943f5d77aeaf8e5e7b4c1
jaafar <jaafarferes@gmail.com>
2023-05-02 12:06:12 +0100

    first commit

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,20 @@
+interface Student {
+    firstName: string
+    lastName: string
+    age: number
+    location: string
+  }
+  const student1: Student = {
+      firstName: "",
+      lastName: "",
+      age: 0,
+      location: ""
+  };
+  
+  const student2: Student = {
+    firstName: "",
+    lastName: "",
+    age: 0,
+    location: ""
+  };
+ const studentsList: Student[] = [student1, student2];
