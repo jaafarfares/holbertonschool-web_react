900b8f8eb033127244a8120b635ba9c40c7379c4
jaafar <jaafarferes@gmail.com>
2023-06-04 15:54:02 +0100

    Animation

--- 0x05-React_inline_styling/task_3/dashboard/src/Notifications/Notifications.js
+++ 0x05-React_inline_styling/task_3/dashboard/src/Notifications/Notifications.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite';
+import { StyleSheet, css, keyframes } from 'aphrodite';
 import NotificationItem from './NotificationItem';
+
+const fadeIn = keyframes({
+  '0%': {
+    opacity: 0.5,
+  },
+  '100%': {
+    opacity: 1,
+  },
+});
+
+const bounce = keyframes({
+  '0%': {
+    transform: 'translateY(0px)',
+  },
+  '50%': {
+    transform: 'translateY(-5px)',
+  },
+  '100%': {
+    transform: 'translateY(5px)',
+  },
+});
 
 const styles = StyleSheet.create({
   notifications: {
     border: '2px dashed #f5f5f5ab',
-    padding: '0', // Remove padding
+    padding: '10px',
     marginBottom: '15px',
   },
-  panelOpen: {
-    position: 'fixed',
-    top: '0',
-    left: '0',
-    right: '0',
-    bottom: '0',
-    background: 'white',
-    zIndex: '999',
-    padding: '20px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: '20px', // Set font size to 20px
+  menuItem: {
+    float: 'right',
+    backgroundColor: '#fff8f8',
+    cursor: 'pointer',
+    ':hover': {
+      animationName: [fadeIn, bounce],
+      animationDuration: '1s, 0.5s',
+      animationIterationCount: '3, 3',
+    },
   },
 });
 
@@ -50,7 +65,7 @@ export default class Notifications extends Component {
   constructor(props) {
     super(props);
     this.markAsRead = this.markAsRead.bind(this);
-    this.previousLength = 0; 
+    this.previousLength = 0;
   }
 
   shouldComponentUpdate(nextProps) {
@@ -63,35 +78,33 @@ export default class Notifications extends Component {
 
   render() {
     const { displayDrawer, listNotifications } = this.props;
-    this.previousLength = listNotifications.length; 
+    this.previousLength = listNotifications.length;
 
     return (
       <div className={css(styles.notifications)}>
-        <div className={css(displayDrawer && styles.panelOpen)}>
-          <div className="NotificationsComponent">
-            <div className="menuItem">
-              <p className={css(styles.text)}>Your notifications</p>
+        <div className="NotificationsComponent">
+          {displayDrawer && (
+            <div className="Notifications">
+              {listNotifications.length === 0 ? (
+                <p>No new notification for now</p>
+              ) : (
+                <ul>
+                  {listNotifications.map((notification) => (
+                    <NotificationItem
+                      key={notification.id}
+                      id={notification.id}
+                      type={notification.type}
+                      value={notification.value}
+                      html={notification.html}
+                      markAsRead={this.markAsRead}
+                    />
+                  ))}
+                </ul>
+              )}
             </div>
-            {displayDrawer && (
-              <div className="Notifications">
-                {listNotifications.length === 0 ? (
-                  <p>No new notification for now</p>
-                ) : (
-                  <ul className={css(styles.text)}>
-                    {listNotifications.map((notification) => (
-                      <NotificationItem
-                        key={notification.id}
-                        id={notification.id}
-                        type={notification.type}
-                        value={notification.value}
-                        html={notification.html}
-                        markAsRead={this.markAsRead}
-                      />
-                    ))}
-                  </ul>
-                )}
-              </div>
-            )}
+          )}
+          <div className={css(styles.menuItem)}>
+            <p>Your notifications</p>
           </div>
         </div>
       </div>
