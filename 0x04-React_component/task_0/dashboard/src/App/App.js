e0c4b984b22e697518a28f4c786d4dccd802d463
jaafar <jaafarferes@gmail.com>
2023-06-04 12:27:10 +0100

    keep testing

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -1,46 +1,51 @@
+import React, { Component, Fragment } from 'react';w
+import './App.css';
+import Header from '../Header/Header.js';
+import Login from '../Login/Login.js';
+import Footer from '../Footer/Footer.js';
+import Notifications from '../Notifications/Notifications.js';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types'; 
+import { getLatestNotification } from '../utils/utils';
+
 export default class App extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    isLoggedIn: false,
+  };
+
   render() {
-    let {
-      isLoggedIn,
-    } = this.props;
-    let  listCourses = [
+    const { isLoggedIn } = this.props;
+
+    const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
       { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 }
+      { id: 3, name: 'React', credit: 40 },
     ];
-  
-    let  listNotifications = [
+
+    const listNotifications = [
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', html: { __html: getLatestNotification()} }
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
     ];
-  
-  return (
-    <Fragment>
-    <div className="App">
-    <div className="upperside">
 
-    <Notifications listNotifications={listNotifications} />
-      <Header />
-      </div>
+    return (
+      <Fragment>
+        <div className="App">
+          <div className="upperside">
+            <Notifications listNotifications={listNotifications} />
+            <Header />
+          </div>
 
-      {
-            isLoggedIn === false &&
-            <Login />
-          }
-          {
-            isLoggedIn === true &&
-            <CourseList listCourses={listCourses} />
-          }
-      <Footer />
-      </div>
+          {isLoggedIn === false && <Login />}
+          {isLoggedIn === true && <CourseList listCourses={listCourses} />}
+
+          <Footer />
+        </div>
       </Fragment>
-    );  
-  };
-};
-App.propTypes = {
-  isLoggedIn: PropTypes.bool
-};
-App.defaultProps = {
-  isLoggedIn: false,
-};
+    );
+  }
+}
