fb3beb3484a9c66070fbbe8ce26c6181ecacb382
jaafar <jaafarferes@gmail.com>
2023-06-04 12:19:01 +0100

    testing

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -7,26 +7,22 @@ import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
 
-const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
-];
 const listNotifications = [
   { id: 1, type: 'default', value: 'New course available' },
   { id: 2, type: 'urgent', value: 'New resume available' },
   { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
 ];
 
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+
 class App extends Component {
-  static defaultProps = {
-    isLoggedIn: false,
-  };
-
-  static propTypes = {
-    isLoggedIn: PropTypes.bool,
-  };
-
+  constructor(props) {
+    super(props);
+  }
   render() {
     const { isLoggedIn } = this.props;
     return (
@@ -39,5 +35,13 @@ class App extends Component {
     );
   }
 }
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
 
 export default App;
