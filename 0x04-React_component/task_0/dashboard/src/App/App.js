9c2111072b194dfe4cb0574d8242973e35040f26
jaafar <jaafarferes@gmail.com>
2023-06-04 12:16:04 +0100

    testing

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -1,69 +1,43 @@
 import React, { Component, Fragment } from 'react';
-import './App.css';
-import Notifications from '../Notifications/Notifications.js';
-import Header from '../Header/Header.js';
-import Footer from '../Footer/Footer.js';
-import Login from '../Login/Login.js';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+];
 
 class App extends Component {
+  static defaultProps = {
+    isLoggedIn: false,
+  };
+
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  };
+
   render() {
-    const listNotifications = [
-      {
-        id: 1,
-        type: "default",
-        value: "New course available",
-      },
-      {
-        id: 2,
-        type: "urgent",
-        value: "New resume available",
-      },
-      {
-        id: 3,
-        type: "urgent",
-        html: { __html: getLatestNotification() },
-      }
-    ];
-
-    const listCourses = [
-      {
-        id: 1,
-        name: "ES6",
-        credit: 60,
-      },
-      {
-        id: 2,
-        name: "Webpack",
-        credit: 20,
-      },
-      {
-        id: 3,
-        name: "React",
-        credit: 40,
-      },
-    ];
-
-    const isLoggedIn = this.props.isLoggedIn;
-
+    const { isLoggedIn } = this.props;
     return (
       <Fragment>
         <Notifications listNotifications={listNotifications} />
-        <div className='App'>
-          <Header />
-        </div>
-        <div className='App-body'>
-          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-        </div>
-        <div className='App-footer'>
-          <Footer />
-        </div>
+        <Header />
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        <Footer />
       </Fragment>
     );
   }
 }
-
-App.defaultProps = {
-  isLoggedIn: false,
-};
 
 export default App;
