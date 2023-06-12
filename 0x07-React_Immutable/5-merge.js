4118a0aaa142d17eaeabec5df1af9efb5692dc90
jaafar <jaafarferes@gmail.com>
2023-06-12 12:52:52 +0100

    6. Nested merge

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -5,5 +5,5 @@ export function concatElements(page1, page2) {
 }
 
 export function mergeElements(page1, page2) {
-    return (Map(page1).merge(Map(page2)));
+  return (Map(page1).merge(Map(page2)));
 }
