a6b8f4e5499c409853095e356cd71bea87d2dcd5
jaafar <jaafarferes@gmail.com>
2023-06-12 12:52:52 +0100

    6. Nested merge

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,15 +1,19 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
-    const cap = (str) => str.charAt(0).toUpperCase() + str.slice(1);
-    const students = Seq(object)
-      .filter((grade) => grade.score > 70)
-      .map((student) => ({
+  const cap = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+  let Students = Seq(object)
+    .filter((grade) => grade.score > 70)
+    .map((student) => {
+      const result = {
         ...student,
         firstName: cap(student.firstName),
         lastName: cap(student.lastName),
-      }))
-      .toJS();
-  
-    console.log(students);
-  }
+      };
+      return result;
+    });
+
+    Students = Students.toJS();
+
+  console.log(Students);
+}
