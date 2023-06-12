0044809365d17c2a1eff37f06e422f7cd913b9b9
jaafar <jaafarferes@gmail.com>
2023-06-12 12:52:52 +0100

    6. Nested merge

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,5 +1,8 @@
 import { Map } from 'immutable';
 
-export default function mergeDeeplyElements(page1, page2){
-    return (Map(page1).mergeDeep(Map(page2)));
+export default function mergeDeeplyElements(page1, page2) {
+  const first = Map(page1);
+  const second = Map(page2);
+
+  return (first.mergeDeep(second));
 }
