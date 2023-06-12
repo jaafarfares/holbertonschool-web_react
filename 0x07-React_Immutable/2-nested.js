b528486005f5083148cb10eccb511363b820add7
jaafar <jaafarferes@gmail.com>
2023-06-12 12:52:52 +0100

    6. Nested merge

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,15 +1,5 @@
+import { getIn, fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-    let value = object;
-    
-    for (let i = 0; i < array.length; i++) {
-      const key = array[i];
-      
-      if (value === undefined || typeof value !== 'object' || value === null) {
-        return undefined;
-      }
-      
-      value = value[key];
-    }
-    
-    return value;
-  }
+  return getIn(fromJS(object), array, undefined);
+}
