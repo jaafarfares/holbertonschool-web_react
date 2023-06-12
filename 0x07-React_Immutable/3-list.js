7bd141ff72a98b5bdf491f1328c9a8275afc6a2a
jaafar <jaafarferes@gmail.com>
2023-06-12 11:15:37 +0100

    3. List and push

---
+++ 0x07-React_Immutable/3-list.js
@@ -0,0 +1,10 @@
+import Immutable, { List } from 'immutable';
+
+export function getListObject(array) {
+    return Immutable.List(array);
+}
+
+
+export function addElementToList(list, element) {
+   return list.push(element);
+}
