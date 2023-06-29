6686986aca2eb6083eaaeea6c4bed67a41b7661c
jaafar <jaafarferes@gmail.com>
2023-06-29 16:22:12 +0100

    7. Async actions & Thunk middleware

---
+++ 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/App/App.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+import './App.css';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+import { connect } from 'react-redux';
+import AppContext from './AppContext.js';
+import {
+  displayNotificationDrawer,
+  hideNotificationDrawer,
+} from '../actions/uiActionCreators';
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#f5f5f5ab',
+    fontFamily: 'Arial, sans-serif',
+  },
+  headerRow: {
+    backgroundColor: '#deb5b545',
+  },
+  footer: {
+    textAlign: 'center',
+    fontStyle: 'italic',
+    fontSize: '12px',
+    borderTop: '1px solid #ddd',
+    padding: '10px 0',
+    marginTop: '20px',
+  },
+});
+
+class App extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  };
+
+  markNotificationAsRead = (id) => {
+    // Implement your logic to mark the notification as read
+  };
+
+  render() {
+    const {
+      logOut,
+      user,
+      displayDrawer,
+      displayNotificationDrawer,
+      hideNotificationDrawer,
+      listNotifications,
+    } = this.props;
+
+    let listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+
+    return (
+      <AppContext.Provider value={{ user, logOut }}>
+        <Notifications
+          listNotifications={listNotifications}
+          displayDrawer={displayDrawer}
+          handleDisplayDrawer={displayNotificationDrawer}
+          handleHideDrawer={hideNotificationDrawer}
+          markNotificationAsRead={this.markNotificationAsRead}
+        />
+        <div className={css(styles.body)}>
+          <div className={css(styles.headerRow)}>
+            <Header />
+          </div>
+
+          {!user.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={listCourses} />
+            </BodySectionWithMarginBottom>
+          )}
+
+          <Footer className={css(styles.footer)} />
+        </div>
+
+        <BodySection title="News from the School">
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum."
+          </p>
+        </BodySection>
+      </AppContext.Provider>
+    );
+  }
+}
+
+App.propTypes = {
+  logOut: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    isLoggedIn: PropTypes.bool.isRequired,
+  }).isRequired,
+  displayDrawer: PropTypes.bool.isRequired,
+  displayNotificationDrawer: PropTypes.func.isRequired,
+  hideNotificationDrawer: PropTypes.func.isRequired,
+  listNotifications: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      type: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+      html: PropTypes.object,
+    })
+  ).isRequired,
+};
+const mapStateToProps = (state) => {
+  return {
+    user: {
+      isLoggedIn: state.get('isUserLoggedIn'),
+    },
+    displayDrawer: state.get('isNotificationDrawerVisible'),
+    listNotifications: state.get('notifications')?.toJS() || [],
+  };
+};
+
+const mapDispatchToProps = {
+  displayNotificationDrawer,
+  hideNotificationDrawer,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/App/App.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { fromJS } from 'immutable';
+
+import App, { mapStateToProps } from './App';
+import { displayNotificationDrawer, hideNotificationDrawer } from '../actions/uiActionCreators';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Testing the <App /> Component', () => {
+  let wrapper;
+  let displayNotificationDrawerSpy;
+  let hideNotificationDrawerSpy;
+
+  beforeEach(() => {
+    displayNotificationDrawerSpy = sinon.spy();
+    hideNotificationDrawerSpy = sinon.spy();
+    wrapper = shallow(
+      <App
+        displayNotificationDrawer={displayNotificationDrawerSpy}
+        hideNotificationDrawer={hideNotificationDrawerSpy}
+      />
+    );
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should call displayNotificationDrawer when clicking on the menu item', () => {
+    wrapper.find('Notifications').prop('handleDisplayDrawer')();
+    expect(displayNotificationDrawerSpy.calledOnce).to.equal(true);
+  });
+
+  it('should call hideNotificationDrawer when clicking on the close button', () => {
+    wrapper.find('Notifications').prop('handleHideDrawer')();
+    expect(hideNotificationDrawerSpy.calledOnce).to.equal(true);
+  });
+
+  it('should return the right object when passing the state', () => {
+    const state = fromJS({
+      isUserLoggedIn: true,
+      isNotificationDrawerVisible: false,
+    });
+
+    const expectedProps = {
+      isLoggedIn: true,
+      displayDrawer: false,
+    };
+
+    const props = mapStateToProps(state);
+
+    expect(props).to.deep.equal(expectedProps);
+  });
+});
