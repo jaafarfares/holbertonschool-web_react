102da9d6268cfc3446279a7ee75d96572f8943c5
jaafar <jaafarferes@gmail.com>
2023-06-29 14:49:34 +0100

    0. Write mapStateToProps

--- 0x0A-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App/App';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import uiReducer, { initialState } from "./reducers/uiReducer";
+import { Map } from "immutable";
 
 
-render(
-  <React.StrictMode>
+const store = createStore(uiReducer, Map(initialState));
+
+ReactDOM.render(
+<React.StrictMode>
+<Provider store={store}>
+
     <App />
+	</Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
