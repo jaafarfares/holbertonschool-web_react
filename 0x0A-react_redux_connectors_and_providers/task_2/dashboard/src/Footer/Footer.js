f575021ffc0b6a1228f65a8ef2754c12c44983d5
jaafar <jaafarferes@gmail.com>
2023-07-02 17:27:01 +0100

    9. Connect user state to the Footer

--- 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,6 +1,30 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import { connect } from 'react-redux';
+import './Footer.css';
 import { getFullYear, getFooterCopy } from '../utils/utils';
+
+const mapStateToProps = (state) => {
+  return {
+    user: {
+      isLoggedIn: state.get('isUserLoggedIn'),
+    },
+  };
+};
+
+function Footer({ user }) {
+  return (
+    <div className='App-footer'>
+      <p>
+        {getFullYear()} - {getFooterCopy()}
+      </p>
+      <p>{user.isLoggedIn ? 'Logged In' : 'Logged Out'}</p> {/* Display the user login status */}
+    </div>
+  );
+}
+
+export default connect(mapStateToProps)(Footer);
+
+/*
 export default function Footer() {
   return (
     <div className='App-footer'>
@@ -10,3 +34,4 @@ export default function Footer() {
   </div>
   );
 }
+ */
