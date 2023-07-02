e83c63392f2d984f8d8c3dfd0fc0ef5bd639a9c3
jaafar <jaafarferes@gmail.com>
2023-07-02 17:29:34 +0100

    10. Connect Logout action creator to the Header

--- 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
+++ 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { StyleSheet, css } from 'aphrodite';
 import HolbertonLogo from '../assets/holberton_logo.jpg';
+import { logout } from '../redux/actions'; // Assuming the logout action creator is implemented in '../redux/actions'
 
 const styles = StyleSheet.create({
   header: {
@@ -21,11 +23,24 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function Header() {
+const mapStateToProps = (state) => {
+  return {
+    user: state.user // Assuming 'user' is the property in the Redux state that holds the user information
+  };
+};
+
+function Header({ user, logout }) {
+  const handleLogout = () => {
+    logout(); // Dispatch the logout action creator when the user clicks on the link
+  };
+
   return (
     <div className={css(styles.header)}>
       <img className={css(styles.logo)} src={HolbertonLogo} alt='logo' />
       <h1 className={css(styles.title)}>Dashboard</h1>
+      {user.isLoggedIn && <a href="#" onClick={handleLogout}>Logout</a>}
     </div>
   );
 }
+
+export default connect(mapStateToProps, { logout })(Header);
